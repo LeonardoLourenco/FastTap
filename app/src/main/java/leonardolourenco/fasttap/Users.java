529223418fe8d8b20d04373a2d085dea1f0e059a
LeonardoLourenço <leonardo-lourenco@hotmail.com>
2018-05-30 21:03:39 +0100

    Created a new (Business)Class: Users

---
+++ app/src/main/java/leonardolourenco/fasttap/Users.java
@@ -0,0 +1,22 @@
+package leonardolourenco.fasttap;
+
+public class Users {
+    private int id;
+    private String UserName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+}
