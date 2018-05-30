f005fd1e8b11369e208d6ed73e01744a3cee63d7
LeonardoLourenço <leonardo-lourenco@hotmail.com>
2018-05-30 19:49:29 +0100

    Created a new Class: DbTableUsers

---
+++ app/src/main/java/leonardolourenco/fasttap/DbTableUsers.java
@@ -0,0 +1,24 @@
+package leonardolourenco.fasttap;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class DbTableUsers implements BaseColumns {
+    public static final String FIELD_USER_NAME = "UserName";
+    private SQLiteDatabase db;
+
+    public DbTableUsers(SQLiteDatabase db){
+        this.db= db;
+    }
+
+    public void create(){
+        db.execSQL(
+                "CREATE TABLE Users(" +
+                        _ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        FIELD_USER_NAME + " TEXT NOT NULL" +
+                        ")"
+        );
+
+    }
+
+}
