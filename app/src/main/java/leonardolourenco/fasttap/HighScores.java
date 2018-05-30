dff1014e67d35887a876a94e8ce7f6dd6d324264
LeonardoLourenço <leonardo-lourenco@hotmail.com>
2018-05-30 21:07:12 +0100

    Created a new (Business)Class: HighScores

---
+++ app/src/main/java/leonardolourenco/fasttap/HighScores.java
@@ -0,0 +1,76 @@
+package leonardolourenco.fasttap;
+
+public class HighScores {
+    private int id;
+    private int type;
+    private String  High1;
+    private String  High2;
+    private String  High3;
+    private String  High4;
+    private String  High5;
+    private int idUser;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getHigh1() {
+        return High1;
+    }
+
+    public void setHigh1(String high1) {
+        High1 = high1;
+    }
+
+    public String getHigh2() {
+        return High2;
+    }
+
+    public void setHigh2(String high2) {
+        High2 = high2;
+    }
+
+    public String getHigh3() {
+        return High3;
+    }
+
+    public void setHigh3(String high3) {
+        High3 = high3;
+    }
+
+    public String getHigh4() {
+        return High4;
+    }
+
+    public void setHigh4(String high4) {
+        High4 = high4;
+    }
+
+    public String getHigh5() {
+        return High5;
+    }
+
+    public void setHigh5(String high5) {
+        High5 = high5;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+}
