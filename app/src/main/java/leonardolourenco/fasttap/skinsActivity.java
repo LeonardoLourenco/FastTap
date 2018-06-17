2ff77f4dcb19d4138cfc4343a00ac36f8ec5b43b
LeonardoLourenço <leonardo-lourenco@hotmail.com>
2018-06-17 21:21:31 +0100

    Made some adjustments to some buttons

--- app/src/main/java/leonardolourenco/fasttap/skinsActivity.java
+++ app/src/main/java/leonardolourenco/fasttap/skinsActivity.java
@@ -1,5 +1,6 @@
 package leonardolourenco.fasttap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,4 +17,10 @@ public class skinsActivity extends AppCompatActivity {
 
     }
 
+
+
+    public void goToMainActivity(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
 }
